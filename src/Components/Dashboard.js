b1c229bf46d1cbdb056fbe440cf0dec20f5bace6
Ariana Eed <eed@cellact.com>
2024-04-11 16:35:20 +0200

    updated the payment link to redirect to the payment success page

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -3,13 +3,13 @@
 import React from 'react';
 
 function Dashboard() {
-  const paymentLink = 'https://buy.stripe.com/test_7sI17Q6KA1Ei00M144'; // Payment Link URL from Stripe
+  const paymentLink = 'https://buy.stripe.com/test_6oEbMuc4U96K00MdQS';  // Payment Link URL from Stripe
 
   return (
     <div>
       <h1>Gift Card</h1>
       <div className="gift-card">
-        <img className="gift-card-image" src="/pexels-lil-artsy-1390433.jpg" alt="Gift Card" />
+        <img className="gift-card-image" src="/Picture1.png" alt="Gift Card" />
         <a className="buy-button" href={paymentLink} target="_blank" rel="noopener noreferrer">Buy Gift Card</a>
       </div>
     </div>
